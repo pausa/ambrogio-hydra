fd9e77eb7b4d9d8f418e2bf2cc78324d4f8cafcf
pausa <pausatep@gmail.com>
2021-05-22 15:52:53 +0000

    updated version for electron

--- head/electron/vue.config.js
+++ head/electron/vue.config.js
@@ -4,6 +4,8 @@ module.exports = {
       nodeIntegration: true,
       builderOptions: {
         appId: 'dev.pausa.ambrogio.head.electron',
+        // eslint-disable-next-line no-template-curly-in-string
+        artifactName: '${name}-${version}-${os}-${arch}.${ext}',
         win: {
           target: 'portable'
         },
