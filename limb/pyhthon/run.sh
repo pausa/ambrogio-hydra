974e582fc0bbd89063d1efa2296b2a2b216e253a
pausa <pausatep@gmail.com>
2021-05-26 09:21:21 +0000

    - limb/python: improved script latency

--- limb/pyhthon/run.sh
+++ limb/pyhthon/run.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 # Example script to run the docker container
+# The goal is to minimize latency when invoking the command
 AMBROGIO_HOME=~/.config/ambrogio
 mkdir -p $AMBROGIO_HOME
 # Making it secure, as secrets are stored in there
 chmod 700 $AMBROGIO_HOME
 
-docker run --rm \
+docker run -it --rm \
   -u "$(id -u)" \
   -v $AMBROGIO_HOME:/workspace/config \
-  ambrogio-limb-python:0.0.1 "$@"
+  --name ambrogio-limb-python \
+  --entrypoint /bin/bash -d \
+  ambrogio-limb-python:0.0.1 2> /dev/null
+
+docker exec ambrogio-limb-python python main.py "$@"
